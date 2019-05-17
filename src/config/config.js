3f45691d02e8a7e97be8876503b60406a8ba2111
ebenKB <eakbo23@gmail.com>
2019-05-17 13:40:33 +0000

    update gitignore

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,24 @@
-
 // create the congiguration file for the system  critical data
-const config = {
-  port: process.env.port || 8000,
-  db: process.env.MONGODB_URI,
-  host: process.env.host,
-};
+let config = {}
 
+if (process.env.NODE_ENV == "development") {
+  console.log('using the development settings');
+  config = {
+    port: 8000,
+    db: process.env.MONGODB_URI,
+    // host: process.env.host,
+  };
+} else if(process.env.NODE_ENV == "production") {
+  console.log('using the production settings');
+  config = {
+    port: process.env.port,
+    db: process.env.MONGODB_URI,
+  };
+} else {
+  console.log('using default settings')
+  config = {
+    port: process.env.port,
+    db: process.env.MONGODB_URI,
+  };
+}
 export default config;
